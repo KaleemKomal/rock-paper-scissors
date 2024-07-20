919018912536d5e5701fd704899304e0f6f01951
KaleemKomal <164108743+KaleemKomal@users.noreply.github.com>
2024-07-20 15:09:42 +0100

    Human and Computer Choice Functions the function for the computer choice has been completed to pick a random choice out of rock, paper or scissors and the function for human choice takes a prompt response from user and allocates it to one of the three selections and defaults to scissors if a corerect response is not given

--- rps.js
+++ rps.js
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+
+function getComputerChoice() {
+    let rand = Math.random();
+    let computerChoice;
+
+    if (rand < 0.33) {
+        computerChoice = "Rock";
+    }
+    else if (rand < 0.66) {
+        computerChoice = "Paper";
+    }
+    else{
+        computerChoice = "Scissors";
+    }
+
+    return computerChoice;
+}
+
+
+function getHumanChoice() {
+    let picker = prompt("Pick Rock, Paper or Scissors");
+    let humanChoice;
+    
+    if (picker == "Rock"){
+        humanChoice = "Rock";
+    }
+    else if (picker == "Paper"){
+        humanChoice = "Paper";
+    }
+    else{
+        humanChoice = "Scissors";
+    }
+
+    return humanChoice;
+}
+
+
+console.log(getHumanChoice());
